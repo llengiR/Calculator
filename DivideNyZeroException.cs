ea2114f1bb1f742025f82925f86cfd74af9c6f75
FelixG11 <felixgudmundsson@gmail.com>
2025-09-29 16:22:17 +0200

    Update DivideNyZeroException.cs

--- DivideNyZeroException.cs
+++ DivideNyZeroException.cs
@@ -1,34 +1,44 @@
 namespace Calculator.Model.Exceptions
 
 {
-    ///<summary>
-    /// Represents a exeption when trying to divde a number by zero.
-    ///</summary>
+    /// <summary>
+    /// Represents a exeption used when trying to divde a number by zero.
+    /// </summary>
     public class DivideByZeroException : Exception
     {
-        ///<summary>
+        /// <summary>
         /// Gets the left operator i.e the value being divided.
-        ///</summary>
+        /// </summary>
         public double Dividend { get; }
-        ///<summary>
+        
+        /// <summary>
         /// Gets the right operator i.e the divisor. 
         /// Will be zero when the exeption is used.
-        ///</summary>
+        /// </summary>
         public double Divisor { get; }
-
-        // set up error
+        
+        /// <summary>
+        /// Initializes a new instance of <see cref="DivideByZeroException"/> class.
+        /// </summary>
+        /// <param> name="dividend">The value being divided.</param>
+        /// <param name="divisor">The divisor which caused the issue.</param>
         public DivideByZeroException(double dividend, double divisor)
         : base ($"DivideByZeroException: {dividend:F2}/{divisor:F2}")
         {
-            Dividend = dividend; //save left number
-            Divisor = divisor; //save right number (zero)
+            Dividend = dividend; //save left number.
+            Divisor = divisor; //save right number (zero).
         }
 
+        /// <summary>
+        /// Returns a string representation of the exception.
+        /// Displaying the division that caused the error.
+        /// </summary>
+        /// <returns>String with a error message.</returns>
         public override string ToString()
         {
-            // display error
             return $"DivideByZeroException:{Dividend:F2}/{Divisor:F2}";
         }
     }
 }
+
 
