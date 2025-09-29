9f9898c08848f575266b255239afe4ffdb82f2fe
FelixG11 <felixgudmundsson@gmail.com>
2025-09-29 16:43:48 +0200

    Update DivideOperators.cs

--- DivideOperators.cs
+++ DivideOperators.cs
@@ -1,16 +1,26 @@
 namespace Calculator.Model.Tokens
 
 {
-    // handles two numbers by adding them
+    /// <summary>
+    /// An addition operator (+) that adds two numbers together.
+    /// </summary>
     public class SumOperator = Operator:
     {
-        // new addition operator
+        /// <summary>
+        /// Initializes new instance of <see cref="SumOperator"/> class.
+        /// And registers its symbol with the base <see cref="Operator"/>.
+        /// </summary>
         public SumOperator() : base("/");
         {
-
+            
         }
     
-    // add first och second number
+    /// <summary>
+    /// calculates the sum of two numbers.
+    /// </summary>
+    /// <param name="left">The left-hand number.</param>
+    /// <param name="right">The right-hand number.</param>
+    /// <returns>The resulting sum of <param name="left"/> + <param name="right"/>.</returns>
     public override double Calculate(double left, double right)
     {
         return left / right; 
