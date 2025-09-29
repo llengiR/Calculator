8ae2133d1175ff98422f215562734f0e39e18b7f
FelixG11 <felixgudmundsson@gmail.com>
2025-09-29 17:10:26 +0200

    Update InvalidOperationException.cs

--- InvalidOperationException.cs
+++ InvalidOperationException.cs
@@ -1,23 +1,37 @@
 namespace Calculator.Model.Exceptions
 {
+    /// <summary>
+    /// Represents a error that occurs when giving the calculator an invalid input.
+    /// </summary>
     public class InvalidOperationException : Exception
     {
-        //Creates standard error message
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidOperationException"/> class
+        /// with a default error message.
+        /// </summary>
         public InvalidOperationException()
         : base("InvalidOperationException")
         {
         }
 
-        // create error with costum message
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidOperationException"/> class
+        /// with a custom error message.
+        /// </summary>
+        /// <param name="message">Error message explaning the reason for the exception.</param>
         public InvalidOperationException(string message)
         : base(message)
         {
         }
 
-        // shows error message 
+        /// <summary>
+        /// Returns a string the represents the used exception.
+        /// </summary>
+        /// <returns>A string contaning the thrown exception and its message.</returns>
         public override string ToString()
         {
             return Message;
         }
     }
 }
+
