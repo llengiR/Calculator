3615e9ea806d2c256618f4aa8c2f9f502a28d6f5
FelixG11 <felixgudmundsson@gmail.com>
2025-09-30 12:10:48 +0200

    Update Operand.cs

--- Operand.cs
+++ Operand.cs
@@ -1,19 +1,29 @@
 namespace Calculator.Model.Tokens
 
 {
-    // number in RPN
+    /// <summary>
+    /// Number used in the RPN calculations.
+    /// </summary>
     public class Operand : Token
     {
-        // Stores the value of Operand
+        /// <summary>
+        /// The actual number used in the calculator.
+        /// </summary>
         public double Value { get; }
-        
-        //creates new operand with value
+
+        /// <summary>
+        /// Creates new operand with the used value.
+        /// </summary>
+        /// <param name="value">The number to store.</param>
         public Operand(double value)
         {
             Value = value;
         }
 
-        // returns value as string
+        /// <summary>
+        /// Show the number as text.
+        /// </summary>
+        /// <returns>Returns the number as a string.</returns>
         public override string ToString()
         {
             return Value.ToString();
