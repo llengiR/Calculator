a5d39f7a2cb170c22d3093e8831aa70b7f4b2c3a
FelixG11 <felixgudmundsson@gmail.com>
2025-09-30 11:43:27 +0200

    Update InvalidTokenException.cs

--- InvalidTokenException.cs
+++ InvalidTokenException.cs
@@ -1,22 +1,34 @@
 namespace Calculator.Model.Exceptions
 
 {
+    /// <summary>
+    /// Exception that is executed when an invalid token is given in the input.
+    /// </summary>
     public class InvalidTokenException : Exception
     {
-        // Stores bad text that caused the problem
+        /// <summary>
+        /// Stores bad text that caused the problem
+        /// </summary>
         public string InvalidInput { get; }
 
-        // creates the error and saves what text was wrong
+        /// <summary>
+        /// Creates a new <see cref="InvalidTokenException"/> for the invalid token.
+        /// </summary>
+        /// <param name="invalidInput">The token that is invalid.</param>
         public InvalidTokenException(string invalidInput)
         : base ($"InvalidTokenException: '{invalidInput}' is not at valid token")
         {
             InvalidInput = invalidInput; //save bad text
         }
 
-        //shows error message
+        /// <summary>
+        /// Returns a string showing an error message.
+        /// </summary>
+        /// <returns>A string containing the exception and and error message.</returns>
         public override string ToString()
         {
             return $"InvalidTokenException: '{InvalidInput}' is not a valid token"; 
         }
     }
 }
+
