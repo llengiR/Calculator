a6e877898d2abd874118dd9b06c4e90ef56ca72b
FelixG11 <felixgudmundsson@gmail.com>
2025-09-30 11:09:31 +0200

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -3,8 +3,16 @@ using Calculator.Controller;
 
 namespace Calculator
 {
+    /// <summary>
+    /// Application starting point.
+    /// </summary>
     class Calculator
     {
+        /// <summary>
+        /// main entry point for the application.
+        /// </summary>
+        /// <param name="args">Optional command-line arguments passed to the calculator.
+        /// these are forwarded to <see cref="CalculatorController.Run(String[])"/>.</param>
         static void Main(string[] args)
         {
             CalculatorController controller = new CalculatorController();
