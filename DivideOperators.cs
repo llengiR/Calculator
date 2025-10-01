146165216e5b2a4b6bfa3c2477d7e60780e17480
FelixG11 <felixgudmundsson@gmail.com>
2025-10-01 12:20:24 +0200

    Update DivideOperators.cs

--- DivideOperators.cs
+++ DivideOperators.cs
@@ -4,12 +4,12 @@ namespace Calculator.Model.Tokens
 
 {
     /// <summary>
-    /// An addition operator (+) that adds two numbers together.
+    /// An division operator (/) that divides one number by the other.
     /// </summary>
     public class DivideOperator : Operator
     {
         /// <summary>
-        /// Initializes new instance of <see cref="SumOperator"/> class.
+        /// Initializes new instance of <see cref="DivideOperator"/> class.
         /// And registers its symbol with the base <see cref="Operator"/>.
         /// </summary>
         public DivideOperator() : base("/")
@@ -18,11 +18,11 @@ namespace Calculator.Model.Tokens
         }
     
     /// <summary>
-    /// calculates the sum of two numbers.
+    /// calculates the quotient of two numbers.
     /// </summary>
     /// <param name="left">The left-hand number.</param>
     /// <param name="right">The right-hand number.</param>
-    /// <returns>The resulting sum of <param name="left"/> + <param name="right"/>.</returns>
+    /// <returns>The resulting qoutient of <param name="left"/> / <param name="right"/>.</returns>
     public override double Calculate(double left, double right)
     {
         if(right == 0)
