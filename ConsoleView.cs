112850a6c2d487d9a5bb473ab95db130dae4807a
FelixG11 <felixgudmundsson@gmail.com>
2025-10-03 15:17:19 +0200

    Update ConsoleView.cs

--- ConsoleView.cs
+++ ConsoleView.cs
@@ -2,9 +2,15 @@ using System;
 
 namespace Calculator.View
 {
-    // user input
+    /// <summary>
+    /// Reads input from user and returns the result in the console.
+    /// </summary>
     public class ConsoleView
     {
+        /// <summary>
+        /// Gets the RPN expression from the user.
+        /// </summary>
+        /// <returns>The line of text given by the user.</returns>
         public string GetInput()
         {
             //show prompt to user
@@ -13,24 +19,34 @@ namespace Calculator.View
 
             return input;
         }
-         //display calcs
+        /// <summary>
+        /// Displays the calculated result.
+        /// </summary>
+        /// <param name="result">The number calculated that is displayed.</param>
         public void ShowResult(double result)
         {
             Console.WriteLine($"Result: {result:F1}");
         }
-        // display error
+        /// <summary>
+        /// Display error message.
+        /// </summary>
+        /// <param name="exception">The error being displayed.</param>
         public void ShowError(Exception exception)
         {
             Console.WriteLine(exception.Message);
         }
 
-        // display exit message when quit
+        /// <summary>
+        /// Display exit message when quit
+        /// </summary>
         public void ShowExitMessage()
         {
             Console.WriteLine("The user exited the application");
         }
 
-        //display message fro wrong command line 
+        /// <summary>
+        /// Display message from wrong command line 
+        /// </summary>
         public void ShowUsageMessage()
         {
             Console.WriteLine("Syntax: Calculator [source destination]");
